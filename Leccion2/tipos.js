8bdf9a85135378021ca4eeeeb4457837c2c374c9
Scarlet Rosa <eusouscarlett@gmail.com>
2022-09-29 17:25:21 +0200

    tipos primitivos

---
+++ Leccion2/tipos.js
@@ -0,0 +1,28 @@
+
+// number
+1, 2, 3;
+
+// string
+"Hola mundo";
+'Hola mundo';
+`Hola mundo`;
+
+// nulo y undefined
+null;
+undefined;
+
+// null, undefined, 0, false = todos false
+
+if (null) {
+    console.log("cumple");
+}
+else {
+    console.log("no cumple");
+}
+
+if (true) {
+    console.log("cumple");
+}
+else {
+    console.log("no cumple");
+}
