b5cfda60996cbe9ee5c73ef6835e0da7551048ad
Scarlet Rosa <eusouscarlett@gmail.com>
2022-09-29 17:35:54 +0200

    variables y constantes

---
+++ Leccion2/variables-constantes.js
@@ -0,0 +1,32 @@
+/* variables
+
+var variable_;
+let variablelet_;
+const constante_;
+
+*/
+
+var a=1;
+console.log(a);
+
+a=4
+console.log(a);
+
+const constante = "soy una constante";
+console.log(constante);
+
+//constante = "intento cambiar la constante";
+
+//////////////////////
+
+var variable = "soy una VAR"
+    for(var i=0; i<3; i++) {
+        var variable = "soy la segunda VAR avariada"
+    }
+console.log(variable);
+
+let variablelet = "soy una LET"
+    for(var i=0; i<3; i++) {
+        let variablelet = "soy la segunda LET avariada"
+    }
+console.log(variablelet);
