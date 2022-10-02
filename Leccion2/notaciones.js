006ab3e98247ffecf035a11605f33a16319cae60
Scarlet Rosa <eusouscarlett@gmail.com>
2022-10-02 16:45:07 +0200

    notaciones

---
+++ Leccion2/notaciones.js
@@ -0,0 +1,29 @@
+// Notación
+
+// ; -> Delimitar el final de una linea
+//const b = 4
+
+// . -> Se utiliza en los objetos para acceder a los atributos
+// movil.anchura
+
+// [] -> listas, arreglos o arrays
+const a = [1, 2, 3, 4]
+console.log(a[2])
+
+// () -> funciones
+function suma(a, b) {
+    // aqui se escribiria la funcion
+}
+
+// {} -> llaves para objetos, funciones y estructuras de control
+
+const movil = {
+    anchura: 5,
+    altura: 10
+}
+
+if (true) {
+    //contenido aqui
+
+    const c = "hola"
+}
